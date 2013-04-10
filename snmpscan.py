1d20a808f6936ecf82e60990a3fb80fa4de0e9c7
HHSnopek <HHSnopek@gmail.com>
2013-04-10 16:47:18 -0500

    Update

--- snmpscan.py
+++ snmpscan.py
@@ -1,11 +1,9 @@
 #! /usr/bin/env python
 from scapy.all import *
 import argparse
-import os
 import re
 import gevent
-import netaddr
-from gevent.pool import Group
+from netaddr import IPNetwork
 
 
 #  Scans for snmp ports with private.
@@ -14,58 +12,50 @@ def scanner(ipaddr):
 	p = IP(dst=ipaddr)
 	UDP(dport=161, sport=39445)
 	SNMP(community="private", PDU=SNMPget(id=1416992799, varbindlist=[SNMPvarbind(oid=ASN1_OID("1.3.6.1.2.1.1.1.0"))]))
-
-	print >> sys.stderr, "Trying " + ip + " ... "
 	pkt = sr1(p, timeout=1)
-	if pkt and pkt.sprintf("%IP.proto%") != "icmp":	
-		print >> sys.stderr, "OK\n"
+	if pkt and pkt.sprintf("%IP.proto%") != "icmp":
 		p1 = pkt.sprintf("%SNMP.PDU%").split("ASN1_STRING['", 1)
 		p2 = p1[1].split("'", 1)
-		
-		print pkt.sprintf("%IP.src%") + " - " + p2[0] + "\n"
+		subnetnotsecure[p] = pkt.sprintf("%IP.src%") + " - " + p2[0] + "\n"
 	else:
-		print >> sys.stderr, "No reply\n"
-
-# Scan '#' of IP's
-def scanwholeip(times):
-	scannedips = []
-	ipaddress = re.search('(\d+\.\d+\.)(\d+)', subnet)
-	octect1and2 = ipaddress.group(0)
-	for i in range(1, 255):
-		newipaddress = octect1and2 + str(i)
-		scannedips[i] = newipaddress
-		spawngreenlets(newipaddress)
-	return scannedips
+		print subnetsecure[p]
 
 
-# Split scanning into separate processes
-def spawngreenlets(subnet):
+# Split scanning into separate processes.
+def spawngreenlets(ipaddress):
+	subnetsecure = []
+	subnetnotsecure = []
 	threads = []
-	group = Group()
-	for ip in IPNetwork(subnet):
-		thread[i] = gevent.spawn(scanner(ip, outputfile))
+	for ip in IPNetwork(str(subnet)):
+		thread[i] = gevent.spawn(scanner(ip))
 		gevent.sleep(0)
-		group.add(thread[i])
-	group.join()
 	gevent.joinall(threads)
+	return threads
+
+
+# Which IP's to scan.
+def whattoscan(Subnet):
+	print Subnet
+	ipandsubnet = re.search('(\d+\.\d+\.\d+.\d+)(.\d+)', Subnet, flags=0)
+	subnetlist = list(ipandsubnet.group(0).subnet(str(ipandsubnet.group(1))))
+	for ip in subnetlist:
+		spawngreenlets(ip)
 
 
 def scan():
-	spawngreenlets(ipaddress)
+	whattoscan(Subnet)
 	print "snmpscan is complete."
-	if justprint is True:
-		if os.stat('/tmp/snmp_output.txt')[6] == 0:
-			print "Your subnet '" + subnet + "' is secure. Nothing was found under your parameters."
-		else:
-			print "Something was found! The location of your outputfile is " + outputfile
-	if foundips is True:
-		print "These are the IP address's you used:\n"
-		for i in iplist:
-			print ip
+	print "Your subnet '" + Subnet + "' is secure. Nothing was found under your parameters."
 
 # Command-line usage
 parser = argparse.ArgumentParser(description='Scan a network for snmp(UDP port 161).')
-parser.add_argument("subnet", help="Network in subnet notation (example: 127.0.0.0/24)")
+parser.add_argument(
+	"Subnet",
+	type=str,
+	nargs='?',
+	help="Network in subnet notation (example: 127.0.0.0/24)",
+	action='store'
+)
 parser.add_argument(  # Stop - Boolean return
 	'-s',
 	help="Stop scanning after a '#' of node(s) are found.",
@@ -86,3 +76,5 @@ parser.add_argument(  # Verbose - printverbose
 )
 
 args = vars(parser.parse_args())
+Subnet = args['Subnet']
+scan()
